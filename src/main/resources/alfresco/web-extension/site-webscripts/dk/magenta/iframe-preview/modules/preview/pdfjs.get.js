cc2a21d869a0f9d4e753c5edbad091e4e5155411
Andreas Kring <andreas@magenta.dk>
2020-04-01 12:17:14 +0200

    Removed download within the PdfJs preview itself

---
+++ src/main/resources/alfresco/web-extension/site-webscripts/dk/magenta/iframe-preview/modules/preview/pdfjs.get.js
@@ -0,0 +1,9 @@
+var idxDownload;
+for (var i = 0; i < model.toolbarItems.length; i++) {
+    if (model.toolbarItems[i].id == "download") {
+        // model.toolbarItems[i].disabled = true;
+        idxDownload = i;
+    }
+}
+
+model.toolbarItems.splice(idxDownload, 1);
